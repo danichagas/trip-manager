3d6c144a2b0afc7052f39f3603c17300496d1079
danichagas <chagasdaniel788@gmail.com>
2025-05-29 20:11:44 -0300

    feat: testing and inserting information into the database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
+import { PrismaClient } from './generated/prisma'
+
+export const prisma = new PrismaClient({
+  log: ['query']
+})
 
 const app = fastify()
+
+app.get('/create', async () => {
+  await prisma.trip.create({
+    data: {
+      destination: 'Pará',
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  })
+  return 'Registrado com sucesso'
+})
+
+app.get('/registers', async () => {
+  const trips = await prisma.trip.findMany()
+  return trips
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running')
